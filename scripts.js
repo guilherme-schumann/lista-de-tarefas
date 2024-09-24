d9fbf6fe96b71e9f715547a559d0b7cc33f1efdc
Guilherme Schumann <design@b2ml.com.br>
2024-09-23 22:15:06 -0300

    Finalização das funcionalidades de marcar tarefa como concluída e excluir tarefa

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const inputElement = document.querySelector(".new-task-input");
 const addTaskButton = document.querySelector(".new-task-button");
 
+const tasksContainer = document.querySelector('.tasks-container');
+
 const validadeInput = () => inputElement.value.trim().length > 0;
 
 const handleAddTask = () => {
@@ -9,15 +11,52 @@ const handleAddTask = () => {
   if (!inputIsValid) {
     return inputElement.classList.add("error");
   }
+
+  const taskItemContainer = document.createElement('div');
+  taskItemContainer.classList.add('task-item');
+
+  const taskContent = document.createElement('p');
+  taskContent.innerText = inputElement.value;
+
+  taskContent.addEventListener("click", () => handleClick(taskContent));
+
+  const deleteItem = document.createElement('i');
+  deleteItem.classList.add("fa-regular");
+  deleteItem.classList.add("fa-trash-can");
+
+  deleteItem.addEventListener("click", () => handleDeleteClick(taskItemContainer, taskContent));
+
+  taskItemContainer.appendChild(taskContent);
+  taskItemContainer.appendChild(deleteItem);
+
+  tasksContainer.appendChild(taskItemContainer);
+
+  inputElement.value = "";
+};
+
+const handleClick = (taskContent) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    if (task.nodeType === Node.ELEMENT_NODE && task.firstChild) {
+      if (task.firstChild.isSameNode(taskContent)) {
+        task.firstChild.classList.toggle("completed");
+      }
+    }
+  }
+};
+
+const handleDeleteClick = (taskItemContainer) => {
+  taskItemContainer.remove();
 };
 
 const handleInputChange = () => {
   const inputIsValid = validadeInput();
 
   if (inputIsValid) {
-    return inputElement.classList.remove('error');
+    return inputElement.classList.remove("error");
   }
-}
+};
 
 addTaskButton.addEventListener("click", () => handleAddTask());
 
